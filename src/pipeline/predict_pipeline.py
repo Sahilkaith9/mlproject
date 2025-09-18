887c8f705831dbbd65203e07be55e992742dd25a
sahil <sahilkaith095@gmail.com>
2025-09-18 16:33:41 +0530

    final commit

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,64 @@
+from src.exceptions import CustomException
+from src.logger import logging
+import sys
+import os
+from src.utils import load_object
+import pandas as pd
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+    def predict(self,feature):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
+            model=load_object(model_path)
+            preprocessor=load_object(preprocessor_path)
+            data_scaled=preprocessor.transform(feature)
+            preds=model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+
+
+
+class CustomData:
+    def __init__(self,gender,race_ethnicity,
+        parental_level_of_education,
+        lunch,
+        test_preparation_course,
+        reading_score,
+        writing_score):
+
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+    
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+        
